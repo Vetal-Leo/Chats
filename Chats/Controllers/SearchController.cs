40643cef575ae4c406fa453d3e275d7a0d2c3ca1
Vitaliy <vt.rubin@mail.ru>
2019-08-10 12:15:13 +0300

    create search page

---
+++ Chats/Controllers/SearchController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Chats.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chats.Controllers
+{
+    public class SearchController : Controller
+    {
+        private ApplicationDbContext db;
+        public SearchController(ApplicationDbContext context)
+        {
+            db = context;
+        }
+        [Authorize]
+        public async Task<IActionResult> Search(int id)
+        {
+            var topics = await db.Topics.ToListAsync();
+            var topic = topics[id - 1].Topic;
+            ViewData["Head"] = topic;
+            return View();
+        }
+    }
+}
